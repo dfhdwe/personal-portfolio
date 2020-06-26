189ff0f7289e32699632c5faaa804262a583fd18
sichael <mwangbox@gmail.com>
2020-06-26 11:52:36 -0400

    Updates

---
+++ precache-manifest.d26df32f1e87647657d1599e109b7181.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d2fcc5886556f72cfc3fd0430422515",
+    "url": "/personal-portfolio/index.html"
+  },
+  {
+    "revision": "54550eba7c675d366054",
+    "url": "/personal-portfolio/static/css/2.e8348c7c.chunk.css"
+  },
+  {
+    "revision": "02a4ff6924387f9f6d3e",
+    "url": "/personal-portfolio/static/css/main.f21b9ddd.chunk.css"
+  },
+  {
+    "revision": "54550eba7c675d366054",
+    "url": "/personal-portfolio/static/js/2.3a30cd1a.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/personal-portfolio/static/js/2.3a30cd1a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a4ff6924387f9f6d3e",
+    "url": "/personal-portfolio/static/js/main.44d45810.chunk.js"
+  },
+  {
+    "revision": "dafb6275d1965e5ba9be",
+    "url": "/personal-portfolio/static/js/runtime-main.b328d4e0.js"
+  },
+  {
+    "revision": "4afab1f5cd21214614942acfcfa0817d",
+    "url": "/personal-portfolio/static/media/cloud_mountain.4afab1f5.jpg"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/personal-portfolio/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/personal-portfolio/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/personal-portfolio/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/personal-portfolio/static/media/slick.f97e3bbf.svg"
+  }
+]);
