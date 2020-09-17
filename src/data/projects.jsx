ece9643bd1c04cead01d39890454b92a0e58a86c
sichael <mwangbox@gmail.com>
2020-09-17 15:19:55 -0400

    added more projects

--- src/data/projects.jsx
+++ src/data/projects.jsx
@@ -4,6 +4,16 @@ const projects = [
         description: "An investigation on US pollution",
         link: "https://tphamdo.github.io/US-Pollution-dataset/",
         languages: ["Python"]
+    }, {
+        name: "Distribution Fitts' Law",
+        description: "HCI research paper on distribution Fitts' Law",
+        link: "https://new.precisionconference.com/user/login",
+        languages: ["java", "python", "research paper"]
+    }, {
+        name: "Video Chat with ScreenShare",
+        description: "Video chat built with WebRTC",
+        link: "https://github.com/dfhdwe/ScreenShare",
+        languages: ["react", "socket.io"]
     }
 ]
 
